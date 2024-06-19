45aeee0bdfd6a26ff1094d7d039eb0fd5fd5d805
Pratik More <pratikmore408@gmail.com>
2024-06-19 18:24:15 +0530

    nitofication route and controller added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/users.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 import connectMongoDB from "./database/connectMongoDB.js";
 
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.get("/", (req, res) => {
   res.send("server is ready");
